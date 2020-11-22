c28a99184ff283f7a9d187c4c8cf08ed2a831ae7
jonathan gatti <jonathangatti2@gmail.com>
2020-11-22 11:01:34 +0000

    sorted googleAuth

--- src/components/GoogleAuth.tsx
+++ src/components/GoogleAuth.tsx
@@ -1,19 +1,30 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const clientId = '400044498365-5e4388knj15icbuqtblgusqhiomtl74u.apps.googleusercontent.com'
 
 function GoogleAuth(): JSX.Element {
+  let [isSignedIn, setIsSignedIn] = useState<boolean>(false);
+
   useEffect(():void => {
-    window.gapi.load('cliant:auth2', (): void => {
+    window.gapi.load('client:auth2', (): void => {
       window.gapi.client.init({
         clientId: clientId,
         scope: 'email'
-      });
+      }).then((): void => {
+        let auth = window.gapi.auth2.getAuthInstance();
+        setIsSignedIn(isSignedIn = auth.isSignedIn.get())
+      })
     })
   },[])
+
+  const renderAuthButton = (): JSX.Element => {
+    if(!isSignedIn) return <div>Not signed in </div>;
+    else return <div>I am signed in</div>;
+  }
+  
   return (
     <div>
-      Google Auth
+      {renderAuthButton()}
     </div>
   )
 }
