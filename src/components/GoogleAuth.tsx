33842a86187b8ac249691ebbcd454ede30f7b50b
jonathan gatti <jonathangatti2@gmail.com>
2020-11-22 11:34:54 +0000

    sign in and out btns

--- src/components/GoogleAuth.tsx
+++ src/components/GoogleAuth.tsx
@@ -3,7 +3,7 @@ import React, {useEffect, useState} from 'react';
 const clientId = '400044498365-5e4388knj15icbuqtblgusqhiomtl74u.apps.googleusercontent.com'
 
 function GoogleAuth(): JSX.Element {
-  let [isSignedIn, setIsSignedIn] = useState<boolean>(false);
+  let [isSignedIn, setIsSignedIn] = useState<boolean | null>(null);
 
   useEffect(():void => {
     window.gapi.load('client:auth2', (): void => {
@@ -12,16 +12,41 @@ function GoogleAuth(): JSX.Element {
         scope: 'email'
       }).then((): void => {
         let auth = window.gapi.auth2.getAuthInstance();
-        setIsSignedIn(isSignedIn = auth.isSignedIn.get())
+        setIsSignedIn(isSignedIn = auth.isSignedIn.get());
+        auth.isSignedIn.listen(onAuthChange);
       })
     })
   },[])
 
-  const renderAuthButton = (): JSX.Element => {
-    if(!isSignedIn) return <div>Not signed in </div>;
-    else return <div>I am signed in</div>;
+  const onAuthChange = (): void => {
+    let auth = window.gapi.auth2.getAuthInstance();
+    setIsSignedIn(isSignedIn = auth.isSignedIn.get());
   }
   
+  const handleSignInButton = (): void => {
+    window.gapi.auth2.getAuthInstance().signIn()
+  }
+
+  const handleSignOutButton = (): void => {
+    window.gapi.auth2.getAuthInstance().signOut()
+  }
+
+  const renderAuthButton = (): JSX.Element | undefined => {
+    if(isSignedIn === null) return undefined;
+    else if (isSignedIn) return (
+      <button onClick={handleSignOutButton} className='ui red google button'>
+        <i className='google icon'/>
+        Sign Out
+      </button>
+    )
+    else if (!isSignedIn) return (
+      <button onClick={handleSignInButton} className='ui green google button'>
+        <i className='google icon'/>
+        Sign In
+      </button>
+    );
+  }
+
   return (
     <div>
       {renderAuthButton()}
