a08dd6c9a08179a7f8c5cd46800b1bf570cf6701
jonathan gatti <jonathangatti2@gmail.com>
2020-12-15 18:08:03 +0000

    edit stream mapStateToProps

--- src/components/streams/StreamEdit.tsx
+++ src/components/streams/StreamEdit.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { editStream } from '../../actions';
 
-function StreamEdit(): JSX.Element{
-  return (
-    <div>
-      stream edit
-    </div>
-  )
+function StreamEdit(): JSX.Element {
+  return <div>stream edit</div>;
 }
 
-export default StreamEdit;
+const mapStateToProps = (state: any, ownProps: any) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+export default connect(mapStateToProps, { editStream })(StreamEdit);
