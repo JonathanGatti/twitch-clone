f1ea1bb32c6193370720810f749469bba31ad991
jonathan gatti <jonathangatti2@gmail.com>
2020-12-21 15:35:57 +0000

    delete stream component

--- src/components/streams/StreamDelete.tsx
+++ src/components/streams/StreamDelete.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Modal from '../Modal';
+import { deleteStream, fetchStream } from '../../actions';
+import { connect } from 'react-redux';
 import history from '../../history';
 
-function StreamDelete({ match }: any): JSX.Element {
+function StreamDelete({ match, deleteStream }: any): JSX.Element {
   const actions = (
     <>
-      <button className="ui button negative">Delete</button>
-      <button className="ui button ">Cancel</button>
+      <button
+        className="ui button negative"
+        onClick={() => {
+          deleteStream(match.params.id);
+          history.push('/');
+        }}
+      >
+        Delete
+      </button>
+      <button onClick={() => history.push('/')} className="ui button ">
+        Cancel
+      </button>
     </>
   );
   return (
@@ -22,4 +34,4 @@ function StreamDelete({ match }: any): JSX.Element {
   );
 }
 
-export default StreamDelete;
+export default connect(null, { fetchStream, deleteStream })(StreamDelete);
