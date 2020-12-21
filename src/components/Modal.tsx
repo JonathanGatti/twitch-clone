ed38d69d9a4944695d1dbfb9cc046f0af990b96f
jonathan gatti <jonathangatti2@gmail.com>
2020-12-21 15:12:41 +0000

    made Modal component reusable

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import reactDOM from 'react-dom';
+import history from '../history';
 
-const Modal = () => {
+interface ModalProps {
+  title: string;
+  content: string;
+  actions: JSX.Element;
+}
+
+const Modal = ({ title, content, actions }: ModalProps) => {
   const modal = document.querySelector('#modal');
 
   return reactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">jasbdjsabdjsa</div>
+    <div
+      onClick={() => history.push('/')}
+      className="ui dimmer modals visible active"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="ui standard modal visible active"
+      >
+        <div className="header">{title}</div>
+        <div className="content">{content}</div>
+        <div className="actions">{actions}</div>
+      </div>
     </div>,
     modal!
   );
